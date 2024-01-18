5842f456a68a55561d57259ddaa51af30688e6fb
vsypko <vsypko.dev@gmail.com>
2024-01-18 15:46:15 +0100

    getflights query

--- src/controlers/contract.controler.ts
+++ src/controlers/contract.controler.ts
@@ -1,36 +1,44 @@
-import { Request, Response, NextFunction } from "express"
-import db from "../db/db.js"
-import { IContract } from "../types.js"
+import { Request, Response, NextFunction } from 'express'
+import db from '../db/db.js'
+import { IContract } from '../types.js'
 
-export async function createContract(req: Request, res: Response, next: NextFunction) {
+export async function createContract(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
-    if (!req.body) throw { staus: 400, data: "Bad request" }
+    if (!req.body) throw { staus: 400, data: 'Bad request' }
     const { airport, airline, supplier, airline_signatory } = req.body
     const createdContract = await db.query(
-      "INSERT INTO contracts (airport, airline, supplier, airline_signatory) VALUES($1, $2, $3, $4) RETURNING *",
-      [airport, airline, supplier, airline_signatory],
+      'INSERT INTO contracts (airport, airline, supplier, airline_signatory) VALUES($1, $2, $3, $4) RETURNING *',
+      [airport, airline, supplier, airline_signatory]
     )
-    if (!createdContract.rowCount) throw { staus: 400, data: "Bad request" }
-    res.send({ data: "Contract created" })
+    if (!createdContract.rowCount) throw { staus: 400, data: 'Bad request' }
+    res.send({ data: 'Contract created' })
   } catch (e) {
     next(e)
   }
 }
 
-export async function getContract(req: Request, res: Response, next: NextFunction) {
+export async function getContract(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
-    if (!req.query) throw { status: 400, data: "Bad request" }
+    if (!req.query) throw { status: 400, data: 'Bad request' }
     const { ap, co, cat } = req.query
     let result: any
-    if (cat === "airline") {
+    if (cat === 'airline') {
       result = await db.query(
-        "SELECT ct.*, co.name, co.reg_number, co.country_iso FROM contracts ct INNER JOIN companies co ON ct.supplier = co.id WHERE ct.airport=$1 AND ct.airline=$2",
-        [ap, co],
+        'SELECT ct.*, co.name, co.reg_number, co.country_iso FROM contracts ct INNER JOIN companies co ON ct.supplier = co.id WHERE ct.airport=$1 AND ct.airline=$2',
+        [ap, co]
       )
     } else {
       result = await db.query(
-        "SELECT ct.id, ct.signed_at, ct.airline, ct.supplier, ct.airport, ct.airline_signatory, ct.supplier_signatory, co.name, co.reg_number, co.iata, co.country_iso FROM contracts ct INNER JOIN companies co ON ct.airline = co.id WHERE ct.airport=$1 AND ct.supplier=$2",
-        [ap, co],
+        'SELECT ct.id, ct.signed_at, ct.airline, ct.supplier, ct.airport, ct.airline_signatory, ct.supplier_signatory, co.name, co.reg_number, co.iata, co.country_iso FROM contracts ct INNER JOIN companies co ON ct.airline = co.id WHERE ct.airport=$1 AND ct.supplier=$2',
+        [ap, co]
       )
     }
     const contracts: IContract[] = result.rows
@@ -40,28 +48,38 @@ export async function getContract(req: Request, res: Response, next: NextFunctio
   }
 }
 
-export async function signContract(req: Request, res: Response, next: NextFunction) {
+export async function signContract(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
-    if (!req.body) throw { status: 400, data: "Bad request" }
+    if (!req.body) throw { status: 400, data: 'Bad request' }
     const { id, user } = req.body
     const result = await db.query(
-      "UPDATE contracts SET signed_at=Now(), supplier_signatory=$2 WHERE id=$1 RETURNING *",
-      [id, user],
+      'UPDATE contracts SET signed_at=Now(), supplier_signatory=$2 WHERE id=$1 RETURNING *',
+      [id, user]
     )
     const contract: IContract = result.rows[0]
-    if (!contract) throw { status: 400, data: "Bad request" }
+    if (!contract) throw { status: 400, data: 'Bad request' }
     res.send(contract)
   } catch (e) {
     next(e)
   }
 }
 
-export async function rejectContract(req: Request, res: Response, next: NextFunction) {
+export async function rejectContract(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
-    if (!req.query) throw { status: 400, data: "Bad request" }
-    const result = await db.query("DELETE FROM contracts WHERE id=$1", [req.query.q])
-    if (!result.rowCount) throw { status: 400, data: "Bad request" }
-    res.send({ data: "Contract rejected" })
+    if (!req.query) throw { status: 400, data: 'Bad request' }
+    const result = await db.query('DELETE FROM contracts WHERE id=$1', [
+      req.query.q,
+    ])
+    if (!result.rowCount) throw { status: 400, data: 'Bad request' }
+    res.send({ data: 'Contract rejected' })
   } catch (e) {
     next(e)
   }
--- src/controlers/order.controler.ts
+++ src/controlers/order.controler.ts
@@ -1,16 +1,20 @@
-import { Request, Response, NextFunction } from "express"
-import db from "../db/db.js"
-import { IFlight } from "../types.js"
+import { Request, Response, NextFunction } from 'express'
+import db from '../db/db.js'
+import { IFlight } from '../types.js'
 
-export async function getFlights(req: Request, res: Response, next: NextFunction) {
+export async function getFlights(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
-    if (!req.query) throw { status: 400, data: "Bad request" }
-    const { ap: airport, co: company, dtf: datefrom, dtt: dateto } = req.query
+    if (!req.query) throw { status: 400, data: 'Bad request' }
+    const { ap, co, dtf, dtt } = req.query
     let result: any
 
     result = await db.query(
-      `SELECT id, TO_CHAR(date,'YYYY-MM-DD') as date, flight, type, reg, "from", "to", TO_CHAR(std,'HH24:MI') as std, to_char(sta,'HH24:MI') as sta, seats, co_id, co_iata FROM flights WHERE date BETWEEN $3::date AND $4::date AND "from"=$1 AND co_id=$2 ORDER BY date ASC, std ASC`,
-      [airport, company, datefrom, dateto],
+      `SELECT fl.id, TO_CHAR(fl.date,'YYYY-MM-DD') as date, fl.flight, fl.type, fl.reg, fl.from, fl.to, TO_CHAR(fl.std,'HH24:MI') as std, to_char(fl.sta,'HH24:MI') as sta, fl.seats, fl.co_id, fl.co_iata FROM flights fl WHERE fl.date BETWEEN $3::date AND $4::date AND fl.from=$1 AND co_id=$2 ORDER BY fl.date ASC, fl.std ASC`,
+      [ap, co, dtf, dtt]
     )
 
     res.send(result.rows)
