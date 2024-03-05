ab311e29e6345ed800591f0ebf0e4014b103b5d3
vsypko <vsypko.dev@gmail.com>
2024-03-05 12:25:17 +0100

    order queries

--- src/controlers/order.controler.ts
+++ src/controlers/order.controler.ts
@@ -13,11 +13,38 @@ export async function getFlights(
     let result: any
 
     result = await db.query(
-      `SELECT fl.id, TO_CHAR(fl.date,'YYYY-MM-DD') as date, fl.flight, fl.type, fl.reg, fl.from, fl.to, TO_CHAR(fl.std,'HH24:MI') as std, to_char(fl.sta,'HH24:MI') as sta, fl.seats, fl.co_id, fl.co_iata, ft.crew, ft.fc, ft.bc, ft.yc FROM flights fl INNER JOIN fleet ft ON POSITION(fl.reg IN ft.reg)<>0 WHERE fl.date BETWEEN $3::date AND $4::date AND fl.from=$1 AND fl.co_id=$2 ORDER BY fl.date ASC, fl.std ASC`,
+      `SELECT fl.id, TO_CHAR(fl.date,'YYYY-MM-DD') as date, fl.flight, fl.type, fl.reg, fl.from, fl.to, TO_CHAR(fl.std,'HH24:MI') as std, to_char(fl.sta,'HH24:MI') as sta, fl.seats, fl.co_id, fl.co_iata, ft.crew, ft.fc, ft.bc, ft.yc, fl.order_id FROM flights fl INNER JOIN fleet ft ON POSITION(fl.reg IN ft.reg)<>0 WHERE fl.date BETWEEN $3::date AND $4::date AND fl.from=$1 AND fl.co_id=$2 ORDER BY fl.date ASC, fl.std ASC`,
       [ap, co, dtf, dtt]
     )
 
     res.send(result.rows)
+  } catch (e) {
+    next(e)
+  }
+}
+
+export async function setOrder(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    if (!req.body) throw { status: 400, data: 'Bad request' }
+    const { flights, orderId, contractId } = req.body
+    const flightIds = await Promise.all(
+      flights.map(async (flight: number) => {
+        const updatedFlights = await db.query(
+          `UPDATE flights SET order_id = $1::uuid WHERE id=$2 returning*`,
+          [orderId, flight]
+        )
+        return updatedFlights.rows[0].id
+      })
+    )
+    const order = await db.query(
+      `INSERT INTO orders(id, contract_id, flights_qty) VALUES ($1, $2, $3) returning*`,
+      [orderId, contractId, flights.length]
+    )
+    res.send({ flightIds, insertedOrder: order.rows[0] })
   } catch (e) {
     next(e)
   }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,13 @@
-import express from "express"
-import { body } from "express-validator"
-import * as auth from "../controlers/auth.controler.js"
-import * as search from "../controlers/search.controler.js"
-import * as user from "../controlers/user.controler.js"
-import * as company from "../controlers/company.controler.js"
-import * as contract from "../controlers/contract.controler.js"
-import * as order from "../controlers/order.controler.js"
-import { authMiddleware } from "../middlewares/middleware.js"
-import multer from "multer"
+import express from 'express'
+import { body } from 'express-validator'
+import * as auth from '../controlers/auth.controler.js'
+import * as search from '../controlers/search.controler.js'
+import * as user from '../controlers/user.controler.js'
+import * as company from '../controlers/company.controler.js'
+import * as contract from '../controlers/contract.controler.js'
+import * as order from '../controlers/order.controler.js'
+import { authMiddleware } from '../middlewares/middleware.js'
+import multer from 'multer'
 
 const imgFileStorageConfig = (path: string) =>
   multer.diskStorage({
@@ -20,8 +20,8 @@ const imgFileStorageConfig = (path: string) =>
   })
 
 //main routs-------------------------------------------------
-const userImgUpload = multer({ storage: imgFileStorageConfig("uploads/uph") })
-const itemImgUpload = multer({ storage: imgFileStorageConfig("uploads/itm") })
+const userImgUpload = multer({ storage: imgFileStorageConfig('uploads/uph') })
+const itemImgUpload = multer({ storage: imgFileStorageConfig('uploads/itm') })
 const router = express.Router()
 
 const searchRouter = express.Router()
@@ -36,63 +36,78 @@ const contractRouter = express.Router()
 const orderRouter = express.Router()
 
 //add main routes ------------------------------------------------------------
-router.use("/user", userRouter)
-router.use("/search", searchRouter)
-router.use("/company", companyRouter)
-router.use("/contract", contractRouter)
-router.use("/orders", orderRouter)
+router.use('/user', userRouter)
+router.use('/search', searchRouter)
+router.use('/company', companyRouter)
+router.use('/contract', contractRouter)
+router.use('/orders', orderRouter)
 
 //uses routes ---------------------------------------------------------
-companyRouter.use("/items", companyItemsRouter)
-searchRouter.use("/airport", airportRouter)
-searchRouter.use("/users", authMiddleware(4), usersRouter)
-searchRouter.use("/countries", countriesRouter)
+companyRouter.use('/items', companyItemsRouter)
+searchRouter.use('/airport', airportRouter)
+searchRouter.use('/users', authMiddleware(4), usersRouter)
+searchRouter.use('/countries', countriesRouter)
 
 //queries for AUTH_ROUTER---------------------------------------------------------------------
-authRouter.post("/signup", body("email").isEmail(), body("password").isLength({ min: 6, max: 12 }), auth.signup)
-authRouter.post("/signin", body("email").isEmail(), body("password").isLength({ min: 6, max: 12 }), auth.signin)
-authRouter.get("/update", auth.tokenUpdate)
-authRouter.get("/signout", auth.signout)
+authRouter.post(
+  '/signup',
+  body('email').isEmail(),
+  body('password').isLength({ min: 6, max: 12 }),
+  auth.signup
+)
+authRouter.post(
+  '/signin',
+  body('email').isEmail(),
+  body('password').isLength({ min: 6, max: 12 }),
+  auth.signin
+)
+authRouter.get('/update', auth.tokenUpdate)
+authRouter.get('/signout', auth.signout)
 
 //queries for USER_ROUTER---------------------------------------------------------------------
-userRouter.use("/auth", authRouter)
-userRouter.post("/updateurl", userImgUpload.single("image"), user.saveUserPhoto)
-userRouter.get("/geturl/:url", user.getUserPhoto)
-userRouter.delete("/deleteurl/:url", user.removeUserPhoto)
-userRouter.post("/updateprofile", user.updateUserProfile)
+userRouter.use('/auth', authRouter)
+userRouter.post('/updateurl', userImgUpload.single('image'), user.saveUserPhoto)
+userRouter.get('/geturl/:url', user.getUserPhoto)
+userRouter.delete('/deleteurl/:url', user.removeUserPhoto)
+userRouter.post('/updateprofile', user.updateUserProfile)
 
 //queries for AIRPORT_ROUTER------------------------------------------------------------------
-airportRouter.get("/", search.getAirport)
-airportRouter.get("/code", search.getAirportbyCode)
-airportRouter.get("/schedule", search.getSchedule)
+airportRouter.get('/', search.getAirport)
+airportRouter.get('/code', search.getAirportbyCode)
+airportRouter.get('/schedule', search.getSchedule)
 
 //queriws for USERS_ROUTER--------------------------------------------------------------------
-usersRouter.get("/", search.getAllUsers)
+usersRouter.get('/', search.getAllUsers)
 
 //queries for COUNTRIES_ROUTER----------------------------------------------------------------
-countriesRouter.get("/", search.getAllCountries)
+countriesRouter.get('/', search.getAllCountries)
 
 //queries for COMPANIES_ROUTER----------------------------------------------------------------
-companyRouter.post("/", company.createCompany)
-companyRouter.get("/", company.getCompanies)
-companyRouter.get("/airport", company.getCompaniesForAirport)
-companyRouter.get("/:type", company.getCompanyItems)
-companyRouter.post("/:type", company.insertCompanyItems)
-companyRouter.patch("/:type", company.updateCompanyItem)
-companyRouter.delete("/:type", company.deleteCompanyItem)
+companyRouter.post('/', company.createCompany)
+companyRouter.get('/', company.getCompanies)
+companyRouter.get('/airport', company.getCompaniesForAirport)
+companyRouter.get('/:type', company.getCompanyItems)
+companyRouter.post('/:type', company.insertCompanyItems)
+companyRouter.patch('/:type', company.updateCompanyItem)
+companyRouter.delete('/:type', company.deleteCompanyItem)
 
 //queries for COMPANY_ITEM_ROUTER-------------------------------------------------------------
-companyItemsRouter.get("/img/:url", company.getItemImgUrl)
-companyItemsRouter.post("/img/update", itemImgUpload.single("image"), company.updateItemImg)
-companyItemsRouter.delete("/img/:type", company.deleteItemImg)
+companyItemsRouter.get('/img/:url', company.getItemImgUrl)
+companyItemsRouter.post(
+  '/img/update',
+  itemImgUpload.single('image'),
+  company.updateItemImg
+)
+companyItemsRouter.delete('/img/:type', company.deleteItemImg)
 
 //queries for CONTRACT_ROUTER-----------------------------------------------------------------
-contractRouter.get("/", contract.getContract)
-contractRouter.post("/", authMiddleware(2), contract.createContract)
-contractRouter.patch("/", authMiddleware(2), contract.signContract)
-contractRouter.delete("/", authMiddleware(2), contract.rejectContract)
+contractRouter.get('/', contract.getContract)
+contractRouter.post('/', authMiddleware(2), contract.createContract)
+contractRouter.patch('/', authMiddleware(2), contract.signContract)
+contractRouter.delete('/', authMiddleware(2), contract.rejectContract)
 
 //queries for ORDER_ROUTER-------------------------------------------------------------------
-orderRouter.get("/", order.getFlights)
+orderRouter.get('/', order.getFlights)
+orderRouter.post('/', order.setOrder)
 
 export default router
